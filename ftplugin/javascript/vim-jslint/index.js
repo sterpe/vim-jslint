f153b62d7414e1a67e6aa1d0b67fe9439ee87d9e
sterpe <srterpe@gmail.com>
2014-08-28 23:08:42 -0700

    Revert "remove home-built rc loader to use rc pkg"
    This reverts commit d63d2b18f174e0ec0a55b87e076c561a7f9db2d3.

--- ftplugin/javascript/vim-jslint/index.js
+++ ftplugin/javascript/vim-jslint/index.js
@@ -2,33 +2,39 @@
  *
  * index.js */
 
-var JSLINT = require('jslint').load('latest')
-  , conf = require('rc')('jslint', {})
-  , puts = require('util').puts
-  , filepath = process.argv[2]
-  , regex = new RegExp("^#![^\\n]*")
-  ;
+var JSLINT = require('jslint').load('latest'),
+  find = require('./lib/fpath-find.js').find,
+  puts = require('util').puts,
+  filepath = process.argv[2],
+  regex = new RegExp("^#![^\\n]*");
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', function (chunk) {
   "use strict";
 
-  var errors
-    , i
-    ;
+  var options,
+    errors,
+    i;
+
+  options = find(filepath, '.jslintrc', {});
+
+  try {
+    options = JSON.parse(options);
+  } catch (e) {
+    options = {};
+  }
 
   chunk = chunk.replace(regex, "");
 
-  for (i = 0, JSLINT(chunk, conf), errors = JSLINT.errors || [];
-      i < errors.length; i += 1) {
+  for (i = 0, JSLINT(chunk, options),
+          errors = JSLINT.errors || [];
+              i < errors.length; i += 1) {
     if (errors[i]) {
       puts(errors[i].line + ':' + errors[i].character +
         ':' + errors[i].reason);
     }
   }
-
   puts(chunk);
-
 });
 
